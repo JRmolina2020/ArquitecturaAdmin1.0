87fb0201b13f9e75e95bff5cf8e4068a67372af8
rodolfo <santiago66@gmail.com>
2017-09-11 15:26:03 -0500

    1 iteracion

---
+++ view/home/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+<link rel="import" href="../src/link.html">
+</head>
+<body>
+<?php include'../src/bower_components/menus/index.php'; ?>
+
+<div class="container-fluid">
+<div class="well">
+<h1>estas aqui</h1>
+</div>
+</div>
+</body>
+</html>
--- view/src/bower_components/menus/index.php
+++ view/src/bower_components/menus/index.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="import" href="../src/link.html">
 <title>APP</title>
-<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-<link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-<link rel="stylesheet" href="bower_components/morris.js/morris.css">
-<link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
-<link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-<link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
-<link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
-
 <header class="main-header">
 <!-- Logo -->
 <a href="index2.html" class="logo">
@@ -38,14 +23,13 @@
 <ul class="nav navbar-nav">
 <li class="dropdown user user-menu">
 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-<img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+<img src="../assets/avatar.png" class="user-image" alt="User Image">
 <span class="hidden-xs">Alexander Pierce</span>
 </a>
 <ul class="dropdown-menu">
 <!-- User image -->
 <li class="user-header">
-<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
+<img src="../assets/avatar.png" class="img-circle" alt="User Image">
 <p>
 Alexander Pierce - Web Developer
 <small>Member since Nov. 2012</small>
@@ -92,7 +76,7 @@ Alexander Pierce - Web Developer
 <!-- Sidebar user panel -->
 <div class="user-panel">
 <div class="pull-left image">
-<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+<img src="../assets/avatar.png" class="img-circle" alt="User Image">
 </div>
 <div class="pull-left info">
 <p>Alexander Pierce</p>
@@ -158,47 +142,6 @@ Alexander Pierce - Web Developer
 </aside>
 <div class="content-wrapper">
 <section class="content-header">
-<H1>PRINCIPAL</H1>
-</section>
-</div>
-<div class="control-sidebar-bg"></div>
-</div>
-<!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-$.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="bower_components/raphael/raphael.min.js"></script>
-<script src="bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="bower_components/moment/min/moment.min.js"></script>
-<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-</body>
-</html>
+
+
+
